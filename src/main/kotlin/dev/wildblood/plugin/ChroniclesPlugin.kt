535d9bfa91ab5ef06446a855a67e7fd7ee4cd8d7
Gianni Bischoff <Oucori@gmail.com>
2026-01-19 12:32:00 +0100

    chore: Update manifest authors and clean up unused imports in `ChroniclesPlugin`

--- src/main/kotlin/dev/wildblood/plugin/ChroniclesPlugin.kt
+++ src/main/kotlin/dev/wildblood/plugin/ChroniclesPlugin.kt
@@ -1,16 +1,8 @@
 package dev.wildblood.plugin
 
-import com.hypixel.hytale.assetstore.AssetRegistry
-import com.hypixel.hytale.assetstore.event.LoadedAssetsEvent
-import com.hypixel.hytale.assetstore.event.RemovedAssetsEvent
-import com.hypixel.hytale.assetstore.map.DefaultAssetMap
-import com.hypixel.hytale.builtin.crafting.CraftingPlugin
 import com.hypixel.hytale.logger.HytaleLogger
-import com.hypixel.hytale.server.core.asset.type.item.config.CraftingRecipe
-import com.hypixel.hytale.server.core.asset.type.item.config.Item
 import com.hypixel.hytale.server.core.plugin.JavaPlugin
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit
-import java.util.function.Consumer
 
 class ChroniclesPlugin(init: JavaPluginInit) : JavaPlugin(init) {
 
@@ -21,7 +13,5 @@ class ChroniclesPlugin(init: JavaPluginInit) : JavaPlugin(init) {
     override fun setup() {
         LOGGER.atInfo().log("Setting up plugin " + this.name)
         this.commandRegistry.registerCommand(ExampleCommand(this.name, this.manifest.version.toString()))
-
-        LOGGER.atInfo().log("Registered RaceAsset")
     }
 }
