3575fdf67b259302f75a880f08f883a7beb37a57
Gianni Bischoff <Oucori@gmail.com>
2026-01-19 11:25:14 +0100

    feat: Migrate plugin code to Kotlin and rename package to `dev.wildblood.plugin`

---
+++ src/main/kotlin/dev/wildblood/plugin/ChroniclesPlugin.kt
@@ -0,0 +1,28 @@
+package dev.wildblood.plugin
+
+import com.hypixel.hytale.assetstore.AssetRegistry
+import com.hypixel.hytale.assetstore.event.LoadedAssetsEvent
+import com.hypixel.hytale.assetstore.event.RemovedAssetsEvent
+import com.hypixel.hytale.assetstore.map.DefaultAssetMap
+import com.hypixel.hytale.builtin.crafting.CraftingPlugin
+import com.hypixel.hytale.logger.HytaleLogger
+import com.hypixel.hytale.server.core.asset.type.item.config.CraftingRecipe
+import com.hypixel.hytale.server.core.asset.type.item.config.Item
+import com.hypixel.hytale.server.core.plugin.JavaPlugin
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit
+import dev.wildblood.plugin.config.RaceAsset
+import java.util.function.Consumer
+
+class ChroniclesPlugin(init: JavaPluginInit) : JavaPlugin(init) {
+
+    companion object {
+        val LOGGER: HytaleLogger = HytaleLogger.forEnclosingClass()
+    }
+
+    override fun setup() {
+        LOGGER.atInfo().log("Setting up plugin " + this.name)
+        this.commandRegistry.registerCommand(ExampleCommand(this.name, this.manifest.version.toString()))
+
+        LOGGER.atInfo().log("Registered RaceAsset")
+    }
+}
